3b8afcd6b213acb58476bf1ababa0b9c5a11e4f4
gsfranca <giovanasantosdefranca634@gmail.com>
2024-05-27 20:14:29 -0300

    Editar e Excluir

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 const handlebars = require("express-handlebars").engine
 const bodyParser = require("body-parser")
+const Handlebars = require('handlebars');
 
 const { initializeApp, applicationDefault, cert } = require('firebase-admin/app')
 const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore')
@@ -18,6 +19,11 @@ app.set("view engine", "handlebars")
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
+Handlebars.registerHelper('eq', function(a, b) 
+{
+    return a === b;
+});
+  
 app.get("/", function(req, res)
 {
     res.render("create")
@@ -41,7 +47,16 @@ app.get("/read", async function(req, res, )
 })
 
 
-app.get("/delete/:id", function(req, res){})
+app.get("/delete/:id", async function(req, res)
+{
+    agendamentosRef
+    .doc(req.params.id)
+    .delete()
+    .then(function () 
+    {
+        res.redirect('/read');
+    });
+})
 app.post("/create", function(req, res)
 {
     var result = agendamentosRef.add(
@@ -58,11 +73,34 @@ app.post("/create", function(req, res)
         res.redirect('/read')
     })
 })
-app.get("/edit/:id", function(req, res){
-
+app.get("/edit/:id", async function(req, res)
+{
+    const dataSnapshot = await agendamentosRef.doc(req.params.id).get();
+    const data = 
+    {
+        id: dataSnapshot.id,
+        nome: dataSnapshot.get('nome'),
+        telefone: dataSnapshot.get('telefone'),
+        origem: dataSnapshot.get('origem'),
+        data_contato: dataSnapshot.get('data_contato'),
+        observacao: dataSnapshot.get('observacao'),
+    };
+    res.render("edit", { data });
 })
 app.post("/update", function(req, res){
-    
+    var result = db
+      .collection("agendamentos")
+      .doc(req.body.id)
+      .update({
+        nome: req.body.nome,
+        telefone: req.body.telefone,
+        origem: req.body.origem,
+        data_contato: req.body.data_contato,
+        observacao: req.body.observacao,
+      })
+      .then(function () {
+        res.redirect("/read");
+      });
 })
 app.listen(8081, function(){
     console.log("Servidor ativo!")
